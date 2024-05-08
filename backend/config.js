3b33dc987fb0369cb2e1cc67cb82f97d2bcfe7e6
Lasitha Harshana <lasitha6646@gmail.com>
2024-05-08 06:13:52 +0530

    fixed dockerfile for backend

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,7 @@
-export const PORT = 8080;
+export const PORT = 8000;
 
-export const mongoDBURL ='mongodb://127.0.0.1/Musicdb';
-export const JWTPRIVATEKEY ="Abc123!";
-export const SALT = 10 ;
+export const mongoDBURL =
+	'mongodb+srv://lasithah:1234@cluster0.xocguij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// export const mongoDBURL ='mongodb://127.0.0.1/Musicdb';
+export const JWTPRIVATEKEY = 'Abc123!';
+export const SALT = 10;
