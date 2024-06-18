d1009474ee52b6242482c6b592e7b4e086bf958d
Lasitha Harshana <lasitha6646@gmail.com>
2024-06-18 07:09:18 +0530

    fix ports

--- frontend/src/pages/DeleteSong.jsx
+++ frontend/src/pages/DeleteSong.jsx
@@ -5,57 +5,62 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 
-import background from '../image/background-image.jpg'; 
+import background from '../image/background-image.jpg';
 
 const DeleteSong = () => {
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const { enqueueSnackbar } = useSnackbar();
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const { enqueueSnackbar } = useSnackbar();
 
-  const handleDeleteSong = () => {
-    setLoading(true);
-    axios
-      .delete(`http://localhost:8080/Songs/${id}`)
-      .then(() => {
-        setLoading(false);
-        enqueueSnackbar('Song Deleted successfully', { variant: 'success' });
-        navigate('/songhome');
-      })
-      .catch((error) => {
-        setLoading(false);
-        enqueueSnackbar('Error', { variant: 'error' });
-        console.log(error);
-      });
-  };
+	const handleDeleteSong = () => {
+		setLoading(true);
+		axios
+			.delete(`http://localhost:8000/Songs/${id}`)
+			.then(() => {
+				setLoading(false);
+				enqueueSnackbar('Song Deleted successfully', { variant: 'success' });
+				navigate('/songhome');
+			})
+			.catch((error) => {
+				setLoading(false);
+				enqueueSnackbar('Error', { variant: 'error' });
+				console.log(error);
+			});
+	};
 
-  const backgroundImageStyle = {
-    backgroundImage: `url(${background})`, 
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundAttachment: "fixed",
-    height: "100vh", 
-  };
+	const backgroundImageStyle = {
+		backgroundImage: `url(${background})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+		backgroundAttachment: 'fixed',
+		height: '100vh',
+	};
 
-  return (
-    <div style={backgroundImageStyle} className="container mx-auto flex justify-center items-center h-screen">
-      <div className="p-4 bg-gray-800 text-white bg-opacity-80 rounded-lg">
-        <BackButton />
-        <h1 className="text-3xl my-4">Delete Song</h1>
-        {loading ? <Spinner /> : ''}
-        <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl p-4 mx-auto w-80">
-          <h3 className="text-2xl text-gray-300 mb-4">Are You Sure You want to delete this Song?</h3>
+	return (
+		<div
+			style={backgroundImageStyle}
+			className="container mx-auto flex justify-center items-center h-screen"
+		>
+			<div className="p-4 bg-gray-800 text-white bg-opacity-80 rounded-lg">
+				<BackButton />
+				<h1 className="text-3xl my-4">Delete Song</h1>
+				{loading ? <Spinner /> : ''}
+				<div className="flex flex-col items-center border-2 border-sky-400 rounded-xl p-4 mx-auto w-80">
+					<h3 className="text-2xl text-gray-300 mb-4">
+						Are You Sure You want to delete this Song?
+					</h3>
 
-          <button
-            className="p-2 bg-red-600 text-white m-4 w-full hover:bg-red-700"
-            onClick={handleDeleteSong}
-          >
-            Yes, Delete it
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+					<button
+						className="p-2 bg-red-600 text-white m-4 w-full hover:bg-red-700"
+						onClick={handleDeleteSong}
+					>
+						Yes, Delete it
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DeleteSong;
--- frontend/src/pages/EditSong.jsx
+++ frontend/src/pages/EditSong.jsx
@@ -5,116 +5,122 @@ import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 
-import background from '../image/background-image.jpg'; 
+import background from '../image/background-image.jpg';
 
 const EditSong = () => {
-  const [title, setTitle] = useState('');
-  const [artist, setArtist] = useState('');
-  const [album, setAlbum] = useState('');
-  const [publishYear, setPublishYear] = useState('');
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const { enqueueSnackbar } = useSnackbar();
+	const [title, setTitle] = useState('');
+	const [artist, setArtist] = useState('');
+	const [album, setAlbum] = useState('');
+	const [publishYear, setPublishYear] = useState('');
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const { enqueueSnackbar } = useSnackbar();
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(`http://localhost:8080/songs/${id}`)
-      .then((response) => {
-        setArtist(response.data.artist);
-        setAlbum(response.data.album);
-        setPublishYear(response.data.publishYear);
-        setTitle(response.data.title);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setLoading(false);
-        alert('An error happened. Please check the console');
-        console.log(error);
-      });
-  }, []);
+	useEffect(() => {
+		setLoading(true);
+		axios
+			.get(`http://localhost:8000/songs/${id}`)
+			.then((response) => {
+				setArtist(response.data.artist);
+				setAlbum(response.data.album);
+				setPublishYear(response.data.publishYear);
+				setTitle(response.data.title);
+				setLoading(false);
+			})
+			.catch((error) => {
+				setLoading(false);
+				alert('An error happened. Please check the console');
+				console.log(error);
+			});
+	}, []);
 
-  const handleEditSong = () => {
-    const data = {
-      title,
-      artist,
-      album,
-      publishYear,
-    };
-    setLoading(true);
-    axios
-      .put(`http://localhost:8080/songs/${id}`, data)
-      .then(() => {
-        setLoading(false);
-        enqueueSnackbar('Song Edited successfully', { variant: 'success' });
-        navigate('/songhome');
-      })
-      .catch((error) => {
-        setLoading(false);
-        enqueueSnackbar('Error', { variant: 'error' });
-        console.log(error);
-      });
-  };
+	const handleEditSong = () => {
+		const data = {
+			title,
+			artist,
+			album,
+			publishYear,
+		};
+		setLoading(true);
+		axios
+			.put(`http://localhost:8000/songs/${id}`, data)
+			.then(() => {
+				setLoading(false);
+				enqueueSnackbar('Song Edited successfully', { variant: 'success' });
+				navigate('/songhome');
+			})
+			.catch((error) => {
+				setLoading(false);
+				enqueueSnackbar('Error', { variant: 'error' });
+				console.log(error);
+			});
+	};
 
-  const backgroundImageStyle = {
-    backgroundImage: `url(${background})`, 
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    backgroundAttachment: 'fixed',
-    height: '100vh', 
-  };
+	const backgroundImageStyle = {
+		backgroundImage: `url(${background})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+		backgroundAttachment: 'fixed',
+		height: '100vh',
+	};
 
-  return (
-    <div style={backgroundImageStyle} className="container mx-auto flex justify-center items-center h-screen">
-      <div className="p-4 bg-black bg-opacity-80 text-white rounded-lg">
-        <BackButton />
-        <h1 className="text-3xl my-4">Edit Song</h1>
-        {loading ? <Spinner /> : ''}
-        <div className="flex flex-col border-2 border-sky-400 rounded-xl p-4 w-[400px]">
-          <div className="my-4">
-            <label className="text-xl mr-4 text-gray-300">Title</label>
-            <input
-              type="text"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
-            />
-          </div>
-          <div className="my-4">
-            <label className="text-xl mr-4 text-gray-300">Artist</label>
-            <input
-              type="text"
-              value={artist}
-              onChange={(e) => setArtist(e.target.value)}
-              className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
-            />
-          </div>
-          <div className="my-4">
-            <label className="text-xl mr-4 text-gray-300">Album</label>
-            <input
-              type="text"
-              value={album}
-              onChange={(e) => setAlbum(e.target.value)}
-              className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
-            />
-          </div>
-          <div className="my-4">
-            <label className="text-xl mr-4 text-gray-300">Publish Year</label>
-            <input
-              type="number"
-              value={publishYear}
-              onChange={(e) => setPublishYear(e.target.value)}
-              className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
-            />
-          </div>
-          <button className="p-2 bg-blue-400 text-white m-8" onClick={handleEditSong}>
-            Save
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={backgroundImageStyle}
+			className="container mx-auto flex justify-center items-center h-screen"
+		>
+			<div className="p-4 bg-black bg-opacity-80 text-white rounded-lg">
+				<BackButton />
+				<h1 className="text-3xl my-4">Edit Song</h1>
+				{loading ? <Spinner /> : ''}
+				<div className="flex flex-col border-2 border-sky-400 rounded-xl p-4 w-[400px]">
+					<div className="my-4">
+						<label className="text-xl mr-4 text-gray-300">Title</label>
+						<input
+							type="text"
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+							className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
+						/>
+					</div>
+					<div className="my-4">
+						<label className="text-xl mr-4 text-gray-300">Artist</label>
+						<input
+							type="text"
+							value={artist}
+							onChange={(e) => setArtist(e.target.value)}
+							className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
+						/>
+					</div>
+					<div className="my-4">
+						<label className="text-xl mr-4 text-gray-300">Album</label>
+						<input
+							type="text"
+							value={album}
+							onChange={(e) => setAlbum(e.target.value)}
+							className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
+						/>
+					</div>
+					<div className="my-4">
+						<label className="text-xl mr-4 text-gray-300">Publish Year</label>
+						<input
+							type="number"
+							value={publishYear}
+							onChange={(e) => setPublishYear(e.target.value)}
+							className="border-2 border-gray-500 text-gray-900 px-4 py-2 w-full"
+						/>
+					</div>
+					<button
+						className="p-2 bg-blue-400 text-white m-8"
+						onClick={handleEditSong}
+					>
+						Save
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default EditSong;
